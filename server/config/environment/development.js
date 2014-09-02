c600ddc631f45d71d173b6b5f18eb41be6265dca
Christian Stewart <kidovate@gmail.com>
2014-09-02 10:37:51 -0700

    Basic page layout

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,8 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://dev:kapparinokappichino@kahana.mongohq.com:10065/webleague'
+    uri: 'mongodb://localhost/webleague-dev'
+    //uri: 'mongodb://dev:kapparinokappichino@kahana.mongohq.com:10065/webleague'
   },
 
   seedDB: false
