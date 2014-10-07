209c6142defb979039731f7f58aec49f7af336c6
Christian Stewart <kidovate@gmail.com>
2014-10-07 16:26:10 -0400

    Manually add component js to index.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -541,7 +541,7 @@ module.exports = function (grunt) {
         },
         files: {
           '<%= yeoman.client %>/index.html': [
-              ['{.tmp,<%= yeoman.client %>}/{app,components}/**/*.js',
+              ['{.tmp,<%= yeoman.client %>}/app/**/*.js',
                '!{.tmp,<%= yeoman.client %>}/app/app.js',
                '!{.tmp,<%= yeoman.client %>}/{app,components}/**/*.spec.js',
                '!{.tmp,<%= yeoman.client %>}/{app,components}/**/*.poly.*',
