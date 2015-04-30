72cc68a8302571bbb0d8991bc39ccd59f05499a5
Christian Stewart <kidovate@gmail.com>
2015-04-29 22:24:36 -0700

    Move header to translations

--- client/components/translations/english.js
+++ client/components/translations/english.js
@@ -1,5 +1,6 @@
 window.translations.en = {
   'TITLE': 'FACEIT Pro League',
+  'HEAD': 'FACEIT Pro League: S1: April 13-May 13, PrizePool 10k &euro;',
   'Profile': 'Profile',
   'Sign Out': 'Sign Out',
   'Chat': 'Chat',
