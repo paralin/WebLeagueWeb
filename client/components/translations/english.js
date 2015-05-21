170bef9cbe1e6e7dd724456d6ceaf0148d1b8413
Christian Stewart <kidovate@gmail.com>
2015-05-20 17:32:12 -0700

    Update season info

--- client/components/translations/english.js
+++ client/components/translations/english.js
@@ -1,6 +1,6 @@
 window.translations.en = {
   'TITLE': 'FACEIT Pro League',
-  'HEAD': 'FACEIT Pro League: S1: April 13-May 13, PrizePool 10k€',
+  'HEAD': 'FACEIT Pro League: S2: June 2-July 2, PrizePool 20k€',
   'Profile': 'Profile',
   'Sign Out': 'Sign Out',
   'Chat': 'Chat',
