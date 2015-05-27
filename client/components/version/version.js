2b2931975213d87996f63f593a45768b1bf80a02
Christian Stewart <kidovate@gmail.com>
2015-05-27 13:48:06 -0700

    Sort and color team list

--- client/components/version/version.js
+++ client/components/version/version.js
@@ -1,4 +1,4 @@
 (function(){
-  var version = '2.0.8';
+  var version = '2.0.9';
   window.build_id = version;
 })();
--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ if(config.seedDB) { require('./config/seed'); }
 // Setup server
 var app = express();
 
-var version = '2.0.8';
+var version = '2.0.9';
 app.locals.build_id = version;
 
 var server = require('http').createServer(app);
