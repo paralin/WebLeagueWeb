92771ba8f2290ea16f2feb8eae634d4037bf1cd4
Christian Stewart <kidovate@gmail.com>
2015-05-29 16:36:54 -0700

    Chat member is smaller

--- client/components/version/version.js
+++ client/components/version/version.js
@@ -1,4 +1,4 @@
 (function(){
-  var version = '2.0.18';
+  var version = '2.0.19';
   window.build_id = version;
 })();
--- server/version.js
+++ server/version.js
@@ -1,2 +1,2 @@
-var version = '2.0.18';
+var version = '2.0.19';
 module.exports = version;
