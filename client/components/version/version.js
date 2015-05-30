d6db6da138f21c3e342fbe644a40ead054338efa
Christian Stewart <kidovate@gmail.com>
2015-05-29 17:54:30 -0700

    Use borders for teams

--- client/components/version/version.js
+++ client/components/version/version.js
@@ -1,4 +1,4 @@
 (function(){
-  var version = '2.0.23';
+  var version = '2.0.24';
   window.build_id = version;
 })();
--- server/version.js
+++ server/version.js
@@ -1,2 +1,2 @@
-var version = '2.0.23';
+var version = '2.0.24';
 module.exports = version;
