4b49805b4c4b4bab1045c8ca7a03c53216bd2d76
Christian Stewart <kidovate@gmail.com>
2015-06-01 16:39:07 -0700

    Update version

--- client/components/version/version.js
+++ client/components/version/version.js
@@ -1,4 +1,4 @@
 (function(){
-  var version = '2.1.8';
+  var version = '2.1.9';
   window.build_id = version;
 })();
--- server/version.js
+++ server/version.js
@@ -1,2 +1,2 @@
-var version = '2.1.8';
+var version = '2.1.9';
 module.exports = version;
