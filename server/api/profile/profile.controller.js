cd681208e9581d5f55c821955fd0938b0d29ec4f
Christian Stewart <kidovate@gmail.com>
2015-06-04 02:27:44 -0700

    Update leader api

--- server/api/profile/profile.controller.js
+++ server/api/profile/profile.controller.js
@@ -3,7 +3,7 @@
 var _ = require('lodash');
 var User = require('../user/user.model');
 
-var selection = {'profile': 1, 'steam.avatarfull': 1, 'steam.steamid': 1, 'steam.profileurl': 1, 'vouch': 1, 'leagues': 1};
+var selection = {'profile': 1, 'steam.avatarfull': 1, 'steam.steamid': 1, 'steam.profileurl': 1, 'vouch': 1, 'profile.leagues': 1};
 
 // Get list of profiles
 exports.index = function(req, res) {
@@ -20,16 +20,9 @@ exports.index = function(req, res) {
 };
 
 exports.indexLeader = function(req, res) {
-  User.find({vouch: {$exists: true}, "vouch._id": {$exists: true}}).select(selection).exec(function (err, profiles) {
+  User.find({vouch: {$exists: true}}).select(selection).exec(function (err, profiles) {
     if(err) { return handleError(res, err); }
-    var ress = [];
-    profiles.forEach(function(profile){
-      var prof = profile.toObject();
-      prof.profile.totalGames = prof.profile.wins+prof.profile.losses;
-      if(prof.profile.totalGames > 0)
-        ress.push(prof);
-    });
-    return res.status(200).json(ress);
+    return res.status(200).json(profiles);
   });
 };
 
