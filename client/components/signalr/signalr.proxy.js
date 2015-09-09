91bb19808b55f9fb1483f2580ca6ce3421a84b10
Christian Stewart <christian@paral.in>
2015-09-08 23:51:15 -0400

    Update hub proxies

--- client/components/signalr/signalr.proxy.js
+++ client/components/signalr/signalr.proxy.js
@@ -78,27 +78,45 @@
             registerHubProxies(proxies, false);
         });
 
-        proxies['admin'] = this.createHubProxy('admin');
+        proxies['admin'] = this.createHubProxy('admin'); 
         proxies['admin'].client = { };
         proxies['admin'].server = {
             changeResult: function (id, result) {
+            /// <summary>Calls the ChangeResult method on the server-side Admin hub.&#10;Returns a jQuery.Deferred() promise.</summary>
+            /// <param name=\"id\" type=\"\">Server side type is System.UInt64</param>
+            /// <param name=\"result\" type=\"Object\">Server side type is WLNetwork.Matches.Enums.EMatchResult</param>
                 return proxies['admin'].invoke.apply(proxies['admin'], $.merge(["ChangeResult"], $.makeArray(arguments)));
              },
 
             getGameList: function () {
+            /// <summary>Calls the GetGameList method on the server-side Admin hub.&#10;Returns a jQuery.Deferred() promise.</summary>
                 return proxies['admin'].invoke.apply(proxies['admin'], $.merge(["GetGameList"], $.makeArray(arguments)));
              },
 
             killMatch: function (id) {
+            /// <summary>Calls the KillMatch method on the server-side Admin hub.&#10;Returns a jQuery.Deferred() promise.</summary>
+            /// <param name=\"id\" type=\"Object\">Server side type is System.Guid</param>
                 return proxies['admin'].invoke.apply(proxies['admin'], $.merge(["KillMatch"], $.makeArray(arguments)));
              },
 
             recalculateMatch: function (id) {
+            /// <summary>Calls the RecalculateMatch method on the server-side Admin hub.&#10;Returns a jQuery.Deferred() promise.</summary>
+            /// <param name=\"id\" type=\"Number\">Server side type is System.Int64</param>
                 return proxies['admin'].invoke.apply(proxies['admin'], $.merge(["RecalculateMatch"], $.makeArray(arguments)));
              },
 
             resultMatch: function (id, result) {
+            /// <summary>Calls the ResultMatch method on the server-side Admin hub.&#10;Returns a jQuery.Deferred() promise.</summary>
+            /// <param name=\"id\" type=\"Object\">Server side type is System.Guid</param>
+            /// <param name=\"result\" type=\"Object\">Server side type is WLNetwork.Matches.Enums.EMatchResult</param>
                 return proxies['admin'].invoke.apply(proxies['admin'], $.merge(["ResultMatch"], $.makeArray(arguments)));
+             },
+
+            submitMatch: function (id, leagueid) {
+            /// <summary>Calls the SubmitMatch method on the server-side Admin hub.&#10;Returns a jQuery.Deferred() promise.</summary>
+            /// <param name=\"id\" type=\"\">Server side type is System.UInt64</param>
+            /// <param name=\"leagueid\" type=\"String\">Server side type is System.String</param>
+                return proxies['admin'].invoke.apply(proxies['admin'], $.merge(["SubmitMatch"], $.makeArray(arguments)));
              }
         };
 
@@ -106,10 +124,14 @@
         proxies['chat'].client = { };
         proxies['chat'].server = {
             memberListSnapshot: function () {
+            /// <summary>Calls the MemberListSnapshot method on the server-side Chat hub.&#10;Returns a jQuery.Deferred() promise.</summary>
                 return proxies['chat'].invoke.apply(proxies['chat'], $.merge(["MemberListSnapshot"], $.makeArray(arguments)));
              },
 
             sendMessage: function (channel, text) {
+            /// <summary>Calls the SendMessage method on the server-side Chat hub.&#10;Returns a jQuery.Deferred() promise.</summary>
+            /// <param name=\"channel\" type=\"String\">Server side type is System.String</param>
+            /// <param name=\"text\" type=\"String\">Server side type is System.String</param>
                 return proxies['chat'].invoke.apply(proxies['chat'], $.merge(["SendMessage"], $.makeArray(arguments)));
              }
         };
@@ -118,46 +140,64 @@
         proxies['matches'].client = { };
         proxies['matches'].server = {
             cancelChallenge: function () {
+            /// <summary>Calls the CancelChallenge method on the server-side Matches hub.&#10;Returns a jQuery.Deferred() promise.</summary>
                 return proxies['matches'].invoke.apply(proxies['matches'], $.merge(["CancelChallenge"], $.makeArray(arguments)));
              },
 
             challengeResponse: function (accept) {
+            /// <summary>Calls the ChallengeResponse method on the server-side Matches hub.&#10;Returns a jQuery.Deferred() promise.</summary>
+            /// <param name=\"accept\" type=\"\">Server side type is System.Boolean</param>
                 return proxies['matches'].invoke.apply(proxies['matches'], $.merge(["ChallengeResponse"], $.makeArray(arguments)));
              },
 
             createMatch: function (options) {
+            /// <summary>Calls the CreateMatch method on the server-side Matches hub.&#10;Returns a jQuery.Deferred() promise.</summary>
+            /// <param name=\"options\" type=\"Object\">Server side type is WLNetwork.Matches.Methods.MatchCreateOptions</param>
                 return proxies['matches'].invoke.apply(proxies['matches'], $.merge(["CreateMatch"], $.makeArray(arguments)));
              },
 
             finalizeMatch: function () {
+            /// <summary>Calls the FinalizeMatch method on the server-side Matches hub.&#10;Returns a jQuery.Deferred() promise.</summary>
                 return proxies['matches'].invoke.apply(proxies['matches'], $.merge(["FinalizeMatch"], $.makeArray(arguments)));
              },
 
             getAvailableGameList: function () {
+            /// <summary>Calls the GetAvailableGameList method on the server-side Matches hub.&#10;Returns a jQuery.Deferred() promise.</summary>
                 return proxies['matches'].invoke.apply(proxies['matches'], $.merge(["GetAvailableGameList"], $.makeArray(arguments)));
              },
 
             joinMatch: function (id, spec) {
+            /// <summary>Calls the JoinMatch method on the server-side Matches hub.&#10;Returns a jQuery.Deferred() promise.</summary>
+            /// <param name=\"id\" type=\"Object\">Server side type is System.Guid</param>
+            /// <param name=\"spec\" type=\"\">Server side type is System.Boolean</param>
                 return proxies['matches'].invoke.apply(proxies['matches'], $.merge(["JoinMatch"], $.makeArray(arguments)));
              },
 
             kickPlayer: function (steamid) {
+            /// <summary>Calls the KickPlayer method on the server-side Matches hub.&#10;Returns a jQuery.Deferred() promise.</summary>
+            /// <param name=\"steamid\" type=\"String\">Server side type is System.String</param>
                 return proxies['matches'].invoke.apply(proxies['matches'], $.merge(["KickPlayer"], $.makeArray(arguments)));
              },
 
             leaveMatch: function () {
+            /// <summary>Calls the LeaveMatch method on the server-side Matches hub.&#10;Returns a jQuery.Deferred() promise.</summary>
                 return proxies['matches'].invoke.apply(proxies['matches'], $.merge(["LeaveMatch"], $.makeArray(arguments)));
              },
 
             pickPlayer: function (steamid) {
+            /// <summary>Calls the PickPlayer method on the server-side Matches hub.&#10;Returns a jQuery.Deferred() promise.</summary>
+            /// <param name=\"steamid\" type=\"String\">Server side type is System.String</param>
                 return proxies['matches'].invoke.apply(proxies['matches'], $.merge(["PickPlayer"], $.makeArray(arguments)));
              },
 
             startChallenge: function (target) {
+            /// <summary>Calls the StartChallenge method on the server-side Matches hub.&#10;Returns a jQuery.Deferred() promise.</summary>
+            /// <param name=\"target\" type=\"Object\">Server side type is WLNetwork.Challenge.Challenge</param>
                 return proxies['matches'].invoke.apply(proxies['matches'], $.merge(["StartChallenge"], $.makeArray(arguments)));
              },
 
             startMatch: function () {
+            /// <summary>Calls the StartMatch method on the server-side Matches hub.&#10;Returns a jQuery.Deferred() promise.</summary>
                 return proxies['matches'].invoke.apply(proxies['matches'], $.merge(["StartMatch"], $.makeArray(arguments)));
              }
         };
